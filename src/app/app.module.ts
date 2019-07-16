8b2ed8f568555aad617d7f8a56239991b14749a6
Benjamin Layet <ben@henoida.com>
2019-07-16 15:03:37 +0200

    Creating entity data services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
+import {AppComponent} from './app.component';
+import {StoreModule} from '@ngrx/store';
+import {entityConfig} from './entity-metadata';
+import {EntityDataModule} from '@ngrx/data';
+import {EffectsModule} from '@ngrx/effects';
+import {AppEffects} from './app.effects';
 
 @NgModule({
   declarations: [
@@ -11,15 +14,12 @@ import { reducers, metaReducers } from './reducers';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true
-      }
-    }),
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([AppEffects]),
+    EntityDataModule.forRoot(entityConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
